19fcc443d3cbeb35671a855c25e12d0485cb81ad
Davimazoti <davilmazoti07@gmail.com>
2025-04-23 13:41:39 -0300

    EnvioTeste

---
+++ Avaliacao_Usuario/src/main/java/com/example/Avaliacao_Usuario/AvaliacaoUsuarioApplication.java
@@ -0,0 +1,13 @@
+package com.example.Avaliacao_Usuario;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AvaliacaoUsuarioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AvaliacaoUsuarioApplication.class, args);
+	}
+
+}
